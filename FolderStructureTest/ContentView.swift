54b3355ef5c95c2684698ef4de2fb8c2c90ca1df
boztopuz <boztopuz@gmail.com>
2024-11-15 13:09:13 +0300

    First Commit

---
+++ FolderStructureTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FolderStructureTest
+//
+//  Created by Burak Öztopuz on 15.11.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FolderStructureTest/FolderStructureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  FolderStructureTestApp.swift
+//  FolderStructureTest
+//
+//  Created by Burak Öztopuz on 15.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct FolderStructureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
